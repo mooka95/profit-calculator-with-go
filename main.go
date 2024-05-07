1b2a25a655caab1ebdc5dc2c96aa4171d2ad2351
mohamed Rabie <mohamed.osa.22@gmail.com>
2024-05-07 03:01:03 +0300

    pref: calculate profit and profit before tax

--- main.go
+++ main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+	var revene float64
+	var taxRate float64
+	var expenses float64
+	fmt.Println("Please enter revene")
+	fmt.Scan(&revene)
+	fmt.Println("Please enter Tax Rate with %")
+	fmt.Scan(&taxRate)
+	fmt.Println("Please enter expenses")
+	fmt.Scan(&expenses)
+	earningBeforeTax := revene - expenses
+	earningAfterTax := earningBeforeTax - ((taxRate / 100) * earningBeforeTax)
+	ratio := earningBeforeTax / earningAfterTax
+	fmt.Printf("earing before tax is %v AND earning after tax is %v ratio is %v", earningBeforeTax, earningAfterTax, ratio)
 }
